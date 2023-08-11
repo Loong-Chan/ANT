0b1790bfd6dd9dde1af5552a3f75a1149075414a
Gallon <50690357+Loong-Chan@users.noreply.github.com>
2023-08-11 19:16:00 +0800

    Update args_ant.py

--- Args/args_ant.py
+++ Args/args_ant.py
@@ -3,14 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("--dataset", default="photo")
+parser.add_argument("--device_idx", default="0")
 parser.add_argument("--seed", type=int, default=42)
-parser.add_argument("--VAE_hidden_size", type=int, default=256)
+parser.add_argument("--VAE_hidden_size", type=int, default=1024)
 parser.add_argument("--VAE_z_size", type=int, default=8)
 parser.add_argument("--temperature", type=float, default=5000)
-parser.add_argument("--device_idx", default="0")
-parser.add_argument("--dataset", default="photo")
 parser.add_argument("--pretrain_epoch", type=int, default=2000)
-parser.add_argument("--pretrain_lr", type=float, default=0.002)
+parser.add_argument("--pretrain_lr", type=float, default=0.0005)
 parser.add_argument("--verbose", default=True)
 parser.add_argument("--ant_lr", type=float, default=5e-4)
 parser.add_argument("--ant_epoch", type=int, default=2000)
